eabeb60935bb36689cf8994cb9a25357faebe3c1
Leticia Ocada dos Santos <leticiaocada@MacBook-Air-de-Leticia.local>
2025-02-15 00:09:57 -0300

    Implement search recipe

--- app.py
+++ app.py
@@ -2,21 +2,29 @@ from flask import Flask, request, render_template, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('mongodb://root:admin@localhost:27017/')
+client = MongoClient('mongodb://localhost:27017/')
 db = client['recipe-database']
 collection = db['recipes']
 
 @app.route('/register', methods=['POST'])
 def create_recipe():
-    print("oi")
     name = request.form['name']
     description = request.form['description']
     ingredients = request.form.getlist('ingredients')
-    print(name)
-    print(description)
-    for ingredient in ingredients:
-        print(ingredient)
-    return jsonify({"message": "Success"})
+    document = {"name": name, "description": description, "ingredients": ingredients}
+    result = collection.insert_one(document)
+    if result.__acknowledged == True:
+        return jsonify({"message": "Successfull insertion"})
+    else:
+        return jsonify({"message": "Failed insertion"}), 400
+    
+@app.route('/searchrecipe', methods=['POST'])
+def search_recipe():
+    return jsonify({"message": "OK"})
+
+@app.route('/searchrecipe', methods=['GET'])
+def render_recipes():
+    return render_template('recipes.html')
 
 @app.route('/register', methods=['GET'])
 def recipe():
